39f6c693ed32cca73b4434ab24eb16623b79603f
April Mintac Pineda <aprilmintacpineda@gmail.com>
2019-12-06 19:13:07 +0800

    initial setup;

---
+++ .eslintrc.js
@@ -0,0 +1,95 @@
+/** @format */
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    allowImportExportEverywhere: true
+  },
+  rules: {
+    'require-atomic-updates': 0,
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    curly: ['error', 'multi-or-nest', 'consistent'],
+    'linebreak-style': ['error', 'unix'],
+    'no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true
+      }
+    ],
+    'rest-spread-spacing': ['error', 'never'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'prefer-spread': ['error'],
+    'prefer-const': 'error',
+    'no-useless-call': ['error'],
+    'no-trailing-spaces': ['error'],
+    'space-before-blocks': ['error', 'always'],
+    'no-unused-vars': ['error'],
+    'no-floating-decimal': ['error'],
+    'comma-dangle': ['error', 'never'],
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'switch-colon-spacing': [
+      'error',
+      {
+        after: true,
+        before: false
+      }
+    ],
+    'space-unary-ops': [
+      'error',
+      {
+        words: true,
+        nonwords: false
+      }
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always'
+      }
+    ],
+    'keyword-spacing': [
+      'error',
+      {
+        before: true,
+        after: true
+      }
+    ],
+    'key-spacing': [
+      'error',
+      {
+        singleLine: {
+          beforeColon: false,
+          afterColon: true,
+          mode: 'strict'
+        },
+        multiLine: {
+          beforeColon: false,
+          afterColon: true,
+          mode: 'strict'
+        }
+      }
+    ],
+    'generator-star-spacing': [
+      'error',
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+  }
+};
