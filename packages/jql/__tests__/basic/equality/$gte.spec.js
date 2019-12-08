3b7e543eb5d26f9f178088540dce45c821c655e8
April Mintac Pineda <aprilmintacpineda@gmail.com>
2019-12-08 10:33:21 +0800

    added test for querying non-numeric fields;

--- packages/jql/__tests__/basic/equality/$gte.spec.js
+++ packages/jql/__tests__/basic/equality/$gte.spec.js
@@ -5,6 +5,7 @@ const JQLError = require('../../../src/errors/JQLError');
 
 const sampleData = [
   {
+    notNumber: 'abc',
     number1: 10,
     number2: 10,
     number3: {
@@ -18,6 +19,7 @@ const sampleData = [
     number8: [{ number8_1: '10' }, { number8_1: '10' }]
   },
   {
+    notNumber: 'abc',
     number1: 0,
     number2: 0,
     number3: {
@@ -128,6 +130,7 @@ describe('Operator $gte', () => {
     const actualResult = jql(query, sampleData);
     expect(actualResult).toEqual([
       {
+        notNumber: 'abc',
         number1: 10,
         number2: 10,
         number3: {
@@ -141,5 +144,13 @@ describe('Operator $gte', () => {
         number8: [{ number8_1: '10' }, { number8_1: '10' }]
       }
     ]);
+  });
+
+  it('handles querying non-numeric values', () => {
+    expect(jql({
+      notNumber: {
+        $gte: 6
+      }
+    }, sampleData)).toEqual([]);
   });
 });
